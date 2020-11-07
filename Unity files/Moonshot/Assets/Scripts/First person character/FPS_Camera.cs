969038c4542ab2cfba80f08cafb8729875aec9e2
Azrac studios <73284890+Azrac.studios@users.noreply.github.com>
2020-11-07 06:53:36 +0530

    Player movement
    Player movement and a spaceship model has been added

---
+++ Unity files/Moonshot/Assets/Scripts/First person character/FPS_Camera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class FPS_Camera : MonoBehaviour
+{
+    //Variables
+    float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
+    private void Start()
+    {
+        //Locks the cursor in the center of the screen and hides it.
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void Update()
+    {
+        //Get Input
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Apply rotation based on the input
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Unity files/Moonshot/Assets/Scripts/First person character/FPS_Character.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FPS_Character : MonoBehaviour
+{
+    //Variables
+    public CharacterController controller;
+    public float speed = 12f;
+
+    private void Update()
+    {
+        //Get Input
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //Apply movement depending on the input
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
